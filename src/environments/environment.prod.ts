7b7bbf2513c55a7712e284b55c643e24306e792a
Mohammed Altoubi <altoubimohammed@gmail.com>
2020-04-13 10:32:40 +0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,26 +2,9 @@ export const environment = {
   production: true,
   firebase: {
   
-    apiKey: "AIzaSyCc8NICaQjLrYjwfUnvQFiewrGY6MvdzyY",
-    authDomain: "izkishop-com.firebaseapp.com",
-    databaseURL: "https://izkishop-com.firebaseio.com",
-    projectId: "izkishop-com",
-    storageBucket: "izkishop-com.appspot.com",
-    messagingSenderId: "1094206439970",
-    appId: "1:1094206439970:web:b468a1d0fdf816b2b9807b",
-    measurementId: "G-9QG2KEMCLL"
+
   }
 
 };
 
 
-// apiKey: "AIzaSyCR9obWw-XMQBS_argqrREosHnjVxczbi4",
-//   authDomain: "supermareket.firebaseapp.com",
-//     databaseURL: "https://supermareket.firebaseio.com",
-//       projectId: "supermareket",
-//         storageBucket: "supermareket.appspot.com",
-//           messagingSenderId: "928370409088",
-//             appId: "1:928370409088:web:6723c57e592ac5407f7065",
-//               measurementId: "G-H0CBG83HTZ"
-//   }
-// var firebaseConfig = {
